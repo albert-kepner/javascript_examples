6e15c684f4995273b11abba5d5b584a50a9b7a7a
albert-kepner <albert.kepner@outlook.com>
2024-10-25 22:53:29 -0400

    jest3

---
+++ jest3/addOfffsets.test.js
@@ -0,0 +1,28 @@
+function addOffsets(offsetsP, LP) {
+    if (LP.length === 0) {
+        return [];
+    }
+    let offsets = offsetsP.slice(); 
+    let L = LP.slice();
+    let first = L.shift();
+    let offset = offsets.shift();
+    offsets.push(offset);
+    let item = first + offset;
+    
+    let rest = addOffsets(offsets, L);
+    let msg = `L = ${L}, item = ${item}, rest = ${rest}`;
+    console.log(msg);
+    return [item].concat(rest);
+}
+
+
+test('xxx', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const offsets = [1, 2];
+    const result = addOffsets(offsets, arr);
+    console.log(`in test result: ${result}`);
+    expect(result).toEqual([2, 4, 4, 6, 6]);
+});
+
+//
+
